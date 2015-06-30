f4fa9b97b9f37d195e5ceadef3c6712be451785d
student <student@MININT-K2BMMCC>
2015-06-29 21:28:26 -0700

    SD1420 Test

---
+++ ITTMaster/src/com/itt/SeventySixers/SeventySixers.java
@@ -0,0 +1,12 @@
+package com.itt.SeventySixers;
+
+public class SeventySixers {
+
+	public static void main(String[] args) {
+		double SeventySixers = 76.0252175;
+		int oneHundred = 100;
+		double SeventySixersValue = (SeventySixers * oneHundred)/100;
+		System.out.println(SeventySixersValue);
+	}
+
+}
---
+++ ITTMaster/src/com/itt/scanner/Test3.java
@@ -0,0 +1,19 @@
+package com.itt.scanner;
+
+import java.util.Scanner;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer: ");
+		int number = input.nextInt();
+		
+		if (number % 5 == 0)
+			System.out.println("HiFive");
+		if (number % 2 == 0 || number % 3 == 0)
+			System.out.println("Georgia");
+
+	}
+
+}
